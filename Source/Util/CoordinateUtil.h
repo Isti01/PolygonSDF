5cbd84453b78989792f3b6ec1411c153d4c0b545
Isti01 <horvatska01@gmail.com>
2023-02-20 22:55:54 +0100

    Make the panning more accurate

--- Source/Util/CoordinateUtil.h
+++ Source/Util/CoordinateUtil.h
@@ -11,6 +11,7 @@ class CoordinateUtil
   public:
     static float2 screenToSceneSpaceCoordinate(const float4x4 &transform, float2 coords);
     static float2 sceneToScreenSpaceCoordinate(const float4x4 &transform, float2 coords);
+    static float2 screenToSceneSpaceVector(const float4x4 &transform, float2 vector);
 
   private:
     CoordinateUtil() = default;

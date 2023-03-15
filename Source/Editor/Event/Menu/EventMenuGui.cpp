24162fb42b0d3b53fd8693694b975599c7590116
Isti01 <horvatska01@gmail.com>
2023-03-15 13:51:43 +0100

    Prevent flooding the event menu with the same message (closes #5)

--- Source/Editor/Event/Menu/EventMenuGui.cpp
+++ Source/Editor/Event/Menu/EventMenuGui.cpp
@@ -1,5 +1,4 @@
 #include "EventMenuGui.h"
-#include "../../../Util/WithImGuiId.h"
 
 using namespace psdf;
 using namespace Falcor;
@@ -11,15 +10,56 @@ EventMenuGui::SharedPtr EventMenuGui::create()
 
 void EventMenuGui::renderGui(Gui *pGui)
 {
+
     Gui::Window window(pGui, "Event Menu");
 
-    for (const auto &event : mEvents)
+    if (window.button("Clear event history"))
     {
-        window.text(event->getName());
+        mEvents.clear();
     }
+
+    renderCumulatedEvents(window);
 }
 
 void EventMenuGui::addNewEvent(EditorEvent::SharedPtr pEvent)
 {
     mEvents.emplace(mEvents.begin(), std::move(pEvent));
+}
+
+void EventMenuGui::renderCumulatedEvents(Gui::Window &window) const
+{
+    if (mEvents.empty())
+    {
+        return;
+    }
+
+    size_t groupCount = 1;
+    std::string groupText = mEvents[0]->getName();
+
+    for (int i = 1; i < mEvents.size(); i++)
+    {
+        std::string currentText = mEvents[i]->getName();
+        if (groupText == currentText)
+        {
+            groupCount++;
+        }
+        else
+        {
+            window.text(getCumulatedText(groupCount, groupText));
+            groupCount = 1;
+            groupText = currentText;
+        }
+    }
+    window.text(getCumulatedText(groupCount, groupText));
+}
+
+std::string EventMenuGui::getCumulatedText(size_t repeats, const std::string &text) const
+{
+    std::stringstream ss;
+    if (repeats > 1)
+    {
+        ss << '(' << repeats << ") ";
+    }
+    ss << text;
+    return ss.str();
 }

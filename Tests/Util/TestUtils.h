d0732e7e6394447f6bdc602b5c643df8a7f4b855
Isti01 <horvatska01@gmail.com>
2023-05-05 15:21:37 +0200

    Reformat project

--- Tests/Util/TestUtils.h
+++ Tests/Util/TestUtils.h
@@ -22,7 +22,7 @@ class TestUtils
 
     static std::vector<glm::dvec2> readDouble2Vector(const std::string &spaceSeparatedList);
 
-    static Polygon::SharedPtr readPolygon(const std::string& path);
+    static Polygon::SharedPtr readPolygon(const std::string &path);
     static std::vector<psdf::PointRegion> readPointRegionList(const std::string &path);
     static std::vector<psdf::LineRegion> readLineRegionList(const std::string &path);
 

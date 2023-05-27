164501663285de32cc9e828214111cf77cc63620
Isti01 <horvatska01@gmail.com>
2023-05-27 13:51:08 +0200

    Update test name

--- Tests/Editor/Command/TestDeleteOutlineStackCommand.h
+++ Tests/Editor/Command/TestDeleteOutlineStackCommand.h
@@ -5,7 +5,7 @@
 #include "../../../Source/Editor/Command/DeleteOutlineStackCommand.h"
 #include "../../../Source/Editor/Core/Editor.h"
 
-TEST_CASE("Test DeleteGroupStackCommand")
+TEST_CASE("Test DeleteOutlineStackCommand")
 {
     psdf::Outline outline{{{1, 2}, {3, 4}, {5, 1}}};
     auto pShape = psdf::Shape::create({outline, outline});
